46630e0168f1232822650a5ed8143456bfc3a398
Mnimaya <mnimayaaa@gmail.com~>
2023-11-29 21:33:14 +0300

    init: database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-
+import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
-  controllers: [AppController],
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      database: 'nest',
+      models: [],
+      autoLoadModels: true,
+    }),
+  ],
 })
 export class AppModule {}
