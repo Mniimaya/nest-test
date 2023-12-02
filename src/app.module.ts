d0a3f098f95b5b07705297dc91b0262529450d22
Mnimaya <mnimayaaa@gmail.com~>
2023-12-02 20:49:25 +0300

    create: module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,21 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './users/users.model';
+
 @Module({
   controllers: [],
   providers: [],
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.PASSWORD,
+      password: 'root',
       database: process.env.DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
